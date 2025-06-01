26366e6e8010d5615b7e0327126cde895bc7d501
Chamod <chamod.lakmal@pickme.lk>
2025-06-01 12:58:11 +0530

    ai image classifier added

--- ai-image-classifier/src/app/app.component.ts
+++ ai-image-classifier/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageClassifierComponent } from './image-classifier/image-classifier.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageClassifierComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ai-image-classifier';
